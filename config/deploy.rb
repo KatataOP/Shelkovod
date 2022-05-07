c9c1d3fb58a5041966da71d5251ef0fa56857ba2
Kirill Afan <kirka121@gmail.com>
2022-05-07 15:02:51 +0300

    test 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to,       "/home/#{fetch(:user)}/web/Shelkovod/"
 set :puma_bind,       "unix://#{shared_path}/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/pids/puma.state"
 set :puma_pid,        "#{shared_path}/pids/puma.pid"
